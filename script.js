301647184e4422af9cc953516a6c5088e7d4265a
naresh <nareshcena666@gmail.com>
2024-03-06 01:01:15 +0530

    all added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 const h2 = React.createElement('h2', {id: "h2"} , "Topics Covered");
 const p1 = React.createElement('p' , {}, "The following is a list of all the topics we cover in the MDN learning area.")
-const topic1 = React.createElement('p', {className: "topic"} , 'Getting started with the web');
+const topic1 = React.createElement('a', { className: "topic" , href: "href1"} , 'Getting started with the web');
 const topic1text = React.createElement('p', {className: "text" } , 'Provides a practical introduction to web development for complete beginners.');
-const topic2 = React.createElement('p' , {className: "topic"} , 'HTML - Structuring the web');
+const topic2 = React.createElement('a' , {className: "topic" , href: "href2"} , 'HTML - Structuring the web');
 const topic2text = React.createElement('p', {className: "text" } , 'HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.');
-const topic3 = React.createElement('p' , {className: "topic"} , 'CSS - Styling the web');
+const topic3 = React.createElement('a' , {className: "topic" , href: "href3"} , 'CSS - Styling the web');
 const topic3text = React.createElement('p', {className: "text" } , "CSS is the language that we use to control our web content's style and layout, as well as adding behaviour like animation. This topic provides comprehensive coverage of CSS.");
 
 
